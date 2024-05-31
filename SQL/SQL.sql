4db9c9df873b948100afe7a7cbe3dbbfd7f3f721
Emile Corneau-Dulude <2239437@bdeb.qc.ca>
2024-05-30 23:57:57 -0400

    Quelques modifications au code sql et ajout du document word

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -8,10 +8,10 @@ CREATE TABLE sandwichs (
 ALTER TABLE sandwichs ADD CONSTRAINT sandwichs_pk PRIMARY KEY ( nom_sandwich );
 
 -- Insertion des données dans la table sandwichs
-INSERT INTO sandwichs (nom,prix,review) VALUES ("Poulet", 7.99, "73%");
-INSERT INTO sandwichs (nom,prix,review) VALUES ("Jambon Fromage", 7.99, "64%");
-INSERT INTO sandwichs (nom,prix,review) VALUES ("Club", 12.50, "30%");
-INSERT INTO sandwichs (nom,prix,review) VALUES ("Bagel Saumon", 11.99, "93%");
+INSERT INTO sandwichs (nom_sandwich,prix,review) VALUES ('Poulet', 7.99, '73%');
+INSERT INTO sandwichs (nom_sandwich,prix,review) VALUES ('Jambon Fromage', 7.99, '64%');
+INSERT INTO sandwichs (nom_sandwich,prix,review) VALUES ('Club', 12.50, '30%');
+INSERT INTO sandwichs (nom_sandwich,prix,review) VALUES ('Bagel Saumon', 11.99, '93%');
 COMMIT;
 
 SELECT *
@@ -28,10 +28,10 @@ CREATE TABLE desserts (
 ALTER TABLE desserts ADD CONSTRAINT desserts_pk PRIMARY KEY ( nom_dessert );
 
 -- Insertion des données dans la table desserts
-INSERT INTO desserts (nom,prix,review) VALUES ("Tartelette", 3.40, "92%");
-INSERT INTO desserts (nom,prix,review) VALUES ("Mille Feuille", 4.20, "52%");
-INSERT INTO desserts (nom,prix,review) VALUES ("Brownies", 2.00, "74%");
-INSERT INTO desserts (nom,prix,review) VALUES ("Eclair", 4.10, "66%");
+INSERT INTO desserts (nom_dessert,prix,review) VALUES ('Tartelette', 3.40, '92%');
+INSERT INTO desserts (nom_dessert,prix,review) VALUES ('Mille Feuille', 4.20, '52%');
+INSERT INTO desserts (nom_dessert,prix,review) VALUES ('Brownies', 2.00, '74%');
+INSERT INTO desserts (nom_dessert,prix,review) VALUES ('Eclair', 4.10, '66%');
 COMMIT;
 
 SELECT *
@@ -48,9 +48,9 @@ CREATE TABLE congeles (
 ALTER TABLE congeles ADD CONSTRAINT congeles_pk PRIMARY KEY ( nom_congele );
 
 -- Insertion des données dans la table congeles
-INSERT INTO congeles (nom,prix,review) VALUES ("Croissant", 3.40, "81%");
-INSERT INTO congeles (nom,prix,review) VALUES ("Chocolatine", 4.20, "79%");
-INSERT INTO congeles (nom,prix,review) VALUES ("Pate Feuilletee", 2.00, "85%");
+INSERT INTO congeles (nom_congele,prix,review) VALUES ('Croissant', 3.40, '81%');
+INSERT INTO congeles (nom_congele,prix,review) VALUES ('Chocolatine', 4.20, '79%');
+INSERT INTO congeles (nom_congele,prix,review) VALUES ('Pate Feuilletee', 2.00, '85%');
 COMMIT;
 
 SELECT *
@@ -60,15 +60,15 @@ FROM CONGELES;
 -- Création de la table commandes
 CREATE TABLE commandes (
     nom_item     VARCHAR2(30) NOT NULL,
-    prix    NUMBER NOT NULL,
+    prix    NUMBER NOT NULL
 );
 
 ALTER TABLE commandes ADD CONSTRAINT commandes_pk PRIMARY KEY ( nom_item );
 
 -- Insertion des données dans la table commandes
-INSERT INTO commandes (nom,prix) VALUES ("Croissant", 3.40);
-INSERT INTO commandes (nom,prix) VALUES ("Mille Feuille", 4.20);
-INSERT INTO commandes (nom,prix) VALUES ("Bagel Saumon", 11.99);
+INSERT INTO commandes (nom_item,prix) VALUES ('Croissant', 3.40);
+INSERT INTO commandes (nom_item,prix) VALUES ('Mille Feuille', 4.20);
+INSERT INTO commandes (nom_item,prix) VALUES ('Bagel Saumon', 11.99);
 COMMIT;
 
 SELECT *
